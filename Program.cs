9e028417659e2e082f8bda3c3b8864f393b38c6d
IlyaBorisov <1.borisov.ilya@gmail.com>
2019-10-10 13:17:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
-namespace compleadapi
+namespace ASP_NET_CORE_Samples
 {
     public class Program
     {
